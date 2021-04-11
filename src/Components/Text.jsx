327e497944500770bf2bbd42377cfef013bb506b
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-04-11 16:26:19 +0200

    Update text component

--- src/Components/Text.jsx
+++ src/Components/Text.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 const P = styled.p`
-    font-size: 1.6rem;
+    font-size: ${props => props.size || '1.6rem'};
     color: #939393;
     margin: 0; 
+    margin-top: ${props => props.top || "unset"};
+    text-align: ${props => props.align};
 `
 
 const Text = (props) => {
     return (
-        <P>{props.children}</P>
+        <P top={props.top} align={props.align} size={props.size}>{props.children}</P>
     );
 }
 
